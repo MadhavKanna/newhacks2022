36e7fc617cdb19cd324c290389dd848455f29861
tejcapildeo <tjkool500@gmail.com>
2022-11-05 17:17:28 -0400

    added Routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from "./Components/Footer";
 import SignIn from "./Components/SignIn"; 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <SignIn/>
-      <Footer/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/Navbar" element={<Navbar />} />
+      </Routes>
+    </Router>
   );
 }
 
