9dccb81a5e4bbbc97121f9f2b8181357d319a048
Madhav Kanna <madhavkanna3141@gmail.com>
2022-11-05 16:18:08 -0400

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-function Navbar(props){
+function Navbar(props) {
     return (
-    <div className='navbar'></div>)
+        <div className='navbar'>
+            <nav class="navbar bg-light">
+                <div class="container-fluid">
+                    <img src=""/>
+                    <a class="navbar-brand" href="#">QuickTutor</a>
+                </div>
+            </nav>
+
+        </div>)
 }
 
 export default Navbar;
